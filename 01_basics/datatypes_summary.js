4f919c60051509a29cb0c64588fa78f25f10ec37
sumit210136 <sumityadav732001@gmail.com>
2025-08-18 14:16:10 +0000

    data types summary

---
+++ 01_basics/datatypes_summary.js
@@ -0,0 +1,21 @@
+// primitive
+
+// 7 types:string,number,boolean,null,undefined,symbol,bgint
+
+const score=100
+const scoreValue=100.3
+
+const isLoggedIn =false
+const outsideTemp=null
+let userEmail;
+
+const id =symbol('123')
+const anotherId = symbol('123')
+
+console.log(id == anotherId);
+
+
+
+// reference(non primitive)
+
+// array,objects,functions
