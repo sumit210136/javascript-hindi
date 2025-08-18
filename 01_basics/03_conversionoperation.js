a7daab7dbdba9fb19fed5e6476fb038e0fa669f6
sumit210136 <sumityadav732001@gmail.com>
2025-08-18 07:00:50 +0000

    operation

--- 01_basics/03_conversionoperation.js
+++ 01_basics/03_conversionoperation.js
@@ -15,7 +15,7 @@ let valueInNumber=Number(score)
 let isLoggedIn = "sumit"
 
 let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+// console.log(booleanIsLoggedIn)
 
 // 1=> true ; 0=> false
 //""=> false
@@ -24,5 +24,45 @@ console.log(booleanIsLoggedIn)
 let someNumber = 33
 
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// ************************ operations **************************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%3);
+
+
+let str1 ="hello"
+let str2 = " sumit"
+let str3 =str1 +str2
+// console.log(str3);
+
+
+// console.log("1" +2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2 ,num3 
+
+num1=num2=num3 = 2 + 2
+
+let gameCounter = 100
+// gameCounter++;
+++gameCounter;
+console.log(gameCounter);
+
